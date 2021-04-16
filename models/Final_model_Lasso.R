314199aaf4eb42e89c35e37da78ec9b27f19fb07
JanRuffian <77497672+JanRuffian@users.noreply.github.com>
2021-04-16 18:37:34 +0200

    Create Lasso (R)
    Create Lasso model to select variables (R)

---
+++ models/Final_model_Lasso.R
@@ -0,0 +1,79 @@
+# Load Dataset
+HealthData <- read.csv('/Users/janruffner/Desktop/HealthcareFraudCapstoneProject/HealthData.csv')
+
+# Creates dummies for states
+HealthData <- dummy_cols(HealthData, select_columns = 'Majority')
+
+# Create Train/Test data
+set.seed(0)
+trainIndex <- createDataPartition(HealthData$PotentialFraud2, p = .8, 
+                                  list = FALSE, 
+                                  times = 1)
+train <- HealthData[trainIndex,]
+test  <- HealthData[-trainIndex,]
+
+nrow(train)
+nrow(test)
+ncol(train)
+ncol(test)
+
+# Create controls for train and test
+controls <- train %>% select(sum.BeneID, sum.InscClaimAmtReimbursed, mean.Age,
+                             OupatientInpatient2Ratio,
+                             sum.AttendingPhysicians, GenderRatio, sum.Claims, 
+                             sum.ChronicCond_Alzheimer, sum.ChronicCond_Cancer, 
+                             sum.ChronicCond_Heartfailure, sum.ChronicCond_Diabetes, 
+                             sum.ChronicCond_Osteoporasis, sum.ChronicCond_rheumatoidarthritis,
+                             sum.ChronicCond_stroke, sum.ChronicCond_ObstrPulmonary, starts_with("Majority")) %>% select(-"Majority_1")
+
+controls2 <- test %>% select(sum.BeneID, sum.InscClaimAmtReimbursed, mean.Age,
+                             OupatientInpatient2Ratio,
+                             sum.AttendingPhysicians, GenderRatio, sum.Claims, 
+                             sum.ChronicCond_Alzheimer, sum.ChronicCond_Cancer, 
+                             sum.ChronicCond_Heartfailure, sum.ChronicCond_Diabetes, 
+                             sum.ChronicCond_Osteoporasis, sum.ChronicCond_rheumatoidarthritis,
+                             sum.ChronicCond_stroke, sum.ChronicCond_ObstrPulmonary, starts_with("Majority")) %>% select(-"Majority_1")
+
+y_train <- as.factor(train$PotentialFraud2)
+x_train <- data.matrix(controls)
+y_test <- as.factor(test$PotentialFraud2)
+x_test <- data.matrix(controls2)
+
+### Lasso Regression
+#Values of lambda over which to check.
+grid = 10^seq(5, -2, length = 100)
+
+# Normalize train data
+normalize <- function(x){
+  return((x-min(x))/(max(x)-min(x)))
+}
+
+x_train <- as.matrix(as.data.frame(x_train) %>% select(-"Majority"))
+x_train_normalized <- normalize(x_train)
+
+set.seed(0)
+cv.lasso = cv.glmnet(x_train_normalized, as.factor(y_train), lambda = grid, alpha = 1,  family = "binomial", nfolds = 10)
+
+#Best Lambda
+plot(cv.lasso, main = "Lasso Regression\n")
+bestlambda.lasso = cv.lasso$lambda.min
+bestlambda.lasso
+log(bestlambda.lasso)
+
+#
+cv.lasso.bestfit = predict(cv.lasso, s = bestlambda.lasso, newx = x_train, type = "class")
+table(truth = y_train, prediction = cv.lasso.bestfit)
+(3034+365)/4328
+
+predict(cv.lasso, s = bestlambda.lasso, type = "coefficients")
+#Intercept:-2.856482e+00
+#sum.InscClaimAmtReimbursed: 3.472128e+01
+#OupatientInpatient2Ratio:-4.473722e+06
+#sum.ChronicCond_Alzheimer: 9.681919e+03
+#Majority_49: 7.730903e+05
+
+#Fitting the lasso regression. Alpha = 1 for lasso regression.
+lasso.models = glmnet(x_train_normalized, y_train, alpha = 1,   family = "binomial",lambda = grid)
+
+#Visualizing the lasso regression shrinkage.
+plot(lasso.models, xvar = "lambda", label = TRUE, main = "Lasso Regression")
